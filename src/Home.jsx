ab3a84e3fd542726065198d31cd0878ff1929d82
Gaurav <gauravkrdas19@gmail.com>
2025-03-12 19:39:22 +0530

    canged document name

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import Navbar from './PageComponents/Navbar'
+import React, { useEffect } from "react";
+import Navbar from "./PageComponents/Navbar";
 
 function Home() {
+  useEffect(() => {
+    document.title = "Gaurav's Portfolio"; 
+  }, []);
+
   return (
-    <div className='bg-back w-full h-screen'>
-        <Navbar />
+    <div className="bg-back w-full h-screen">
+      <Navbar />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
