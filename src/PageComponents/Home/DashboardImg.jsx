8a5035073537b147f3c913a1c0e4e9f6c2c0d932
Gaurav <gauravkrdas19@gmail.com>
2025-03-16 15:50:53 +0530

    placed img to correct position

--- src/PageComponents/Home/DashboardImg.jsx
+++ src/PageComponents/Home/DashboardImg.jsx
@@ -3,7 +3,7 @@ import profilePic from "../../assets/profilePic.png"
 
 function DashboardImg() {
   return (
-    <div className='absolute right-0 -bottom-41 scale-65 z-0'>
+    <div className='absolute -right-20 -bottom-41 scale-65 z-0'>
         <img src={profilePic}/>
     </div>
   )
