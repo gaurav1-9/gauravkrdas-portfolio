0f9eff0037f8866170bc32e0e026c4480e4041f7
Gaurav <gauravkrdas19@gmail.com>
2025-03-18 22:55:25 +0530

    changed var to const

--- src/PageComponents/Navbar.jsx
+++ src/PageComponents/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 function Navbar() {
-    var [selectedTab,setSelectedTab] = useState(0);
+    const [selectedTab,setSelectedTab] = useState(0);
   return (
     <nav className='flex justify-center items-center'>
         <ul className='flex gap-12 bg-secondaryAccent mt-6 px-4 py-2 z-10 rounded-2xl shadow-[0_0_6px_rgba(0,0,0,0.25)]'>
