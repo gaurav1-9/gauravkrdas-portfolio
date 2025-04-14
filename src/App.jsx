9ca6dfb0ea5e48a8b6ca952b2b74ba9bb0ed520b
Gaurav <gauravkrdas19@gmail.com>
2025-04-14 21:16:27 +0530

    scrolling to section

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Home from './PageComponents/Home/Home'
 import Navbar from './PageComponents/Navbar'
 import Projects from './PageComponents/Projects/Projects'
 import About from './PageComponents/About/About'
 
 const App = () => {
-  return (
-    <div className='bg-back'>
-        <Navbar/>
-        <Home/>
-        <Projects/>
-        <About/>
-    </div>
-  )
+    const homePageRef = useRef(null);
+    const projectPageRef = useRef(null);
+    const aboutPageRef = useRef(null);
+
+    const scrollToSection = (section) => {
+        section.current?.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    return (
+        <div className='bg-back'>
+            <Navbar scrollToSection = {scrollToSection} refs = {{homePageRef, projectPageRef, aboutPageRef}}/>
+            <div ref={homePageRef}><Home /></div>
+            <div ref={projectPageRef}><Projects /></div>
+            <div ref={aboutPageRef}><About /></div>
+        </div>
+    )
 }
 
 export default App
--- src/PageComponents/Navbar.jsx
+++ src/PageComponents/Navbar.jsx
@@ -1,22 +1,39 @@
 import React, { useState } from 'react'
 
-function Navbar() {
+function Navbar({scrollToSection, refs}) {
     const [selectedTab,setSelectedTab] = useState(0);
+
+    function handleClick(index){
+        setSelectedTab(index);
+        switch(index){
+            case 0:
+                console.log(refs)
+                scrollToSection(refs.homePageRef)
+                break;
+            case 1:
+                scrollToSection(refs.projectPageRef)
+                break;
+            case 2:
+                scrollToSection(refs.aboutPageRef)
+                break;
+        }
+    }
+
   return (
     <nav className='flex justify-center items-center sticky top-0 z-100 bg-transparent'>
         <ul className='flex gap-12 bg-secondaryAccent mt-6 px-4 py-2 z-10 rounded-2xl shadow-[0_0_6px_rgba(0,0,0,0.25)]'>
             <li className={`text-2xl ease-in duration-100 cursor-pointer ${(selectedTab===0)? "text-secondary":"text-primary"} hover:scale-[1.03]`} 
-            onClick={()=> setSelectedTab(0)}
+            onClick={()=> handleClick(0)}
             >
                 HOME
             </li>
             <li className={`text-2xl ease-in duration-100 cursor-pointer ${(selectedTab===1)? "text-secondary":"text-primary"} hover:scale-[1.03]`} 
-            onClick={()=> setSelectedTab(1)}
+            onClick={()=> handleClick(1)}
             >
                 PROJECTS
             </li>
             <li className={`text-2xl ease-in duration-100 cursor-pointer ${(selectedTab===2)? "text-secondary":"text-primary"} hover:scale-[1.03]`} 
-            onClick={()=> setSelectedTab(2)}
+            onClick={()=> handleClick(2)}
             >
                 ABOUT
             </li>
