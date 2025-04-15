5de4d9ac4fdbf0581fe22fc0ad764f8efce56e3a
Gaurav <gauravkrdas19@gmail.com>
2025-04-15 20:32:10 +0530

    layout of about page

--- src/PageComponents/About/About.jsx
+++ src/PageComponents/About/About.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import aboutPic from '../../assets/aboutPic.jpg'
+import LeftInfo from './LeftInfo'
 
 const About = () => {
     return (
-        <div className="w-full h-screen">
-            About
+        <div className="w-full h-screen flex">
+            <LeftInfo/>
+            <div className='bg-green-600 h-full w-2/3'></div>
         </div>
     )
 }
---
+++ src/PageComponents/About/LeftInfo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LeftInfo = () => {
+    return (
+        <div className='w-1/3 h-full flex justify-end items-center'>
+            <div className='rounded-full w-90 h-90 border-8 border-secondary shadow-[0_0_8px_rgba(0,0,0,0.6)]'>
+            </div>
+            
+        </div>
+    )
+}
+
+export default LeftInfo
