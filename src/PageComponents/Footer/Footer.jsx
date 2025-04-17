ed08136ed34f3a1c2022775b7abdf0db825fcc5e
Gaurav <gauravkrdas19@gmail.com>
2025-04-17 22:55:06 +0530

    added footer

--- src/PageComponents/Footer/Footer.jsx
+++ src/PageComponents/Footer/Footer.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import { MdOutlineScreenshotMonitor } from "react-icons/md";
+import { FaMobileScreen } from "react-icons/fa6";
+import { FaHeart } from "react-icons/fa";
+import { LiaCopyright } from "react-icons/lia";
 
 const Footer = () => {
+    const width = window.innerWidth;
+    const height = window.innerHeight
+    const year = new Date().getFullYear()
 
     return (
-        <div className='relative bg-primary h-40 p-4 flex'>
-            <div className='h-full w-1/4 bg-green-300'></div>
-            <div className='h-full w-2/4 bg-blue-400'></div>
-            <div className='h-full w-1/4 relative'>
-                <div className='text-primaryAccent text-sm absolute bottom-0 right-0 flex items-center'>
-                    <MdOutlineScreenshotMonitor />
-                    <span className='pl-1'>{window.innerWidth}x{window.innerHeight}</span>
-                </div>
+        <div className='relative bg-primary h-1/12 p-4 flex items-center'>
+            <div className='flex items-center gap-1 text-primaryAccent'>
+                <LiaCopyright />
+                <p>{year}, built with</p>
+                <FaHeart className='text-secondary' aria-label='love' />
+                <p>by GAURAV KUMAR DAS</p>
+            </div>
+
+            <div className='text-primaryAccent text-sm absolute right-5 flex items-center'>
+                {(width > 640) ? <MdOutlineScreenshotMonitor /> : <FaMobileScreen />}
+                <span className='pl-1'>{width}x{height}</span>
             </div>
         </div>
     )
