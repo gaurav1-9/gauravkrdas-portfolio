3ede6d310dc711132575918f2a26e62609ebc9a7
Gaurav <gauravkrdas19@gmail.com>
2025-04-18 00:13:13 +0530

    github chart added

--- src/PageComponents/Projects/Projects.jsx
+++ src/PageComponents/Projects/Projects.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Projects = () => {
     return (
         <div className="w-full h-screen">
-            Projects
+            <div className='flex justify-center bg-tabbedAccent p-4'>
+                <a href="https://github.com/gaurav1-9" target='_blank'>
+                    <img src="https://ghchart.rshah.org/D12E2E/gaurav1-9" alt="" className='pointer scale-120' />
+                </a>
+            </div>
         </div>
     )
 }
