b450a7243e0189af3ef01c9bf455bc73cedf7ad9
Gaurav <gauravkrdas19@gmail.com>
2025-04-18 13:16:34 +0530

    project component added

--- src/PageComponents/Projects/GithubChart.jsx
+++ src/PageComponents/Projects/GithubChart.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
+
 const GithubChart = () => {
     return (
-        <div className=' bg-tabbedAccent p-4 w-fit rounded-2xl shadow-[0_0_8px_rgba(0,0,0,0.25)] scale-[1.3] pointer'>
-            <a href="https://github.com/gaurav1-9" target='_blank' className='flex justify-center flex-col gap-2'>
-                <img src="https://ghchart.rshah.org/D12E2E/gaurav1-9" alt="" className='pointer' />
-                <p className='pointer text-center text-primary font-light'>Code Contribution in GitHub</p>
-            </a>
+        <div className='flex justify-center pt-30'>
+            <div className=' bg-tabbedAccent p-4 w-fit rounded-2xl shadow-[0_0_8px_rgba(0,0,0,0.25)] scale-[1.3] pointer'>
+                <a href="https://github.com/gaurav1-9" target='_blank' className='flex justify-center flex-col gap-2'>
+                    <img src="https://ghchart.rshah.org/D12E2E/gaurav1-9" alt="" className='pointer' />
+                    <p className='pointer text-center text-primary font-light'>Code Contribution in GitHub</p>
+                </a>
+            </div>
         </div>
     )
 }
--- src/PageComponents/Projects/Projects.jsx
+++ src/PageComponents/Projects/Projects.jsx
@@ -1,11 +1,60 @@
 import React from 'react'
 import GithubChart from './GithubChart'
+import { FaGithub } from "react-icons/fa";
+
 
 const Projects = () => {
+    const projectList = [
+        {
+            name: "React Notes App",
+            techStack: [
+                {
+                    icon: "devicon-react-original",
+                    techName: "React"
+                },
+                {
+                    icon: "devicon-nodejs-plain",
+                    techName: "Node.js"
+                },
+                {
+                    icon: "devicon-express-original",
+                    techName: "Express"
+                },
+                {
+                    icon: "devicon-tailwindcss-original",
+                    techName: "TailwindCSS"
+                },
+            ],
+            desc: "A responsive user friendly Notes management app made in React and TailwindCSS with draggable notes card",
+            link: "https://github.com/gaurav1-9/react-notes-app",
+        },
+    ]
     return (
-        <div className="w-full h-screen">
-            <div className='flex justify-center pt-30'>
-                <GithubChart/>
+        <div className="w-full h-fit">
+            <GithubChart />
+            <div className='flex flex-wrap px-10 pt-12 gap-4 justify-center w-full relative'>
+                {projectList.map((item, index) => (
+                    <div key={index} className='w-2/7 bg-tabbedAccent h-65 rounded-2xl flex flex-col items-center py-4'>
+                        <p className='text-secondary font-semibold text-xl'>{item.name}</p>
+                        <p className='text-primary font-light text-center pt-3 px-6'>{item.desc}</p>
+                        <div className='flex flex-wrap gap-2 justify-center pt-2'>
+                            {
+                                item.techStack.map((techItem, idx) => (
+                                    <div className='flex w-fit bg-secondary rounded-xl justify-between px-3 items-center py-1'>
+                                        <i className={`${techItem.icon} pr-2 text-sm`}></i>
+                                        <p className='uppercase font-light text-sm'>{techItem.techName}</p>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                        <a href={item.link} className='bottom-4 absolute'>
+                            <div className='flex items-center pointer'>
+                                <FaGithub className='text-secondary pointer'/>
+                                <p className='pl-1 pointer font-light text-secondary'>Source code</p>
+                            </div>
+                        </a>
+                    </div>
+                ))}
             </div>
         </div>
     )
