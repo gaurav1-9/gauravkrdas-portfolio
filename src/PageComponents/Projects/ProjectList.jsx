0e1cfa0fc46dd38a955534f5ba476f6d444fb1b3
Gaurav <gauravkrdas19@gmail.com>
2025-04-18 13:48:13 +0530

    ProjectList individual component

---
+++ src/PageComponents/Projects/ProjectList.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { FaGithub } from "react-icons/fa";
+
+const ProjectList = ({projList}) => {
+    return (
+        <div className='flex flex-wrap px-10 pt-12 gap-4 justify-center w-full relative'>
+            {projList.map((item, index) => (
+                <div key={index} className='w-2/7 bg-tabbedAccent h-65 rounded-2xl flex flex-col items-center py-4'>
+                    <p className='text-secondary font-semibold text-xl'>{item.name}</p>
+                    <p className='text-primary font-light text-center pt-3 px-6'>{item.desc}</p>
+                    <div className='flex flex-wrap gap-2 justify-center pt-2'>
+                        {
+                            item.techStack.map((techItem, idx) => (
+                                <div key={idx} className='flex w-fit bg-secondary rounded-xl justify-between px-3 items-center py-1'>
+                                    <i className={`${techItem.icon} pr-2 text-sm`}></i>
+                                    <p className='uppercase font-light text-sm'>{techItem.techName}</p>
+                                </div>
+                            ))
+                        }
+                    </div>
+                    <a href={item.link} className='bottom-4 absolute'>
+                        <div className='flex items-center pointer'>
+                            <FaGithub className='text-secondary pointer' />
+                            <p className='pl-1 pointer font-light text-secondary'>Source code</p>
+                        </div>
+                    </a>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default ProjectList
