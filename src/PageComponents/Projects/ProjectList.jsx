71c4a7aae857fd44292b9d4409639d0ad50d4264
Gaurav <gauravkrdas19@gmail.com>
2025-06-05 02:01:30 +0530

    project page resposiveness

--- src/PageComponents/Projects/ProjectList.jsx
+++ src/PageComponents/Projects/ProjectList.jsx
@@ -4,26 +4,26 @@ import { FaLink } from "react-icons/fa6";
 
 const ProjectList = ({ projList }) => {
     return (
-        <div className='flex flex-wrap px-10 pt-12 gap-4 justify-center w-full'>
+        <div className='flex flex-wrap px-10 pt-5 lg:pt-12 gap-2 lg:gap-4 justify-center w-full'>
             {projList.map((item, index) => (
-                <div key={index} className='w-2/7 bg-tabbedAccent h-65 rounded-2xl flex flex-col items-center py-4 relative'>
+                <div key={index} className='lg:w-2/7 bg-tabbedAccent h-fit lg:h-65 rounded-2xl flex flex-col items-center py-4 px-4 lg:px-0 relative'>
                     <div className='flex gap-2 items-center'>
-                        <p className='text-secondary text-center font-semibold text-xl'>{item.name}</p>
+                        <p className='text-secondary text-center font-semibold text-xl leading-5 lg:leading-7'>{item.name}</p>
                         {
                             (item.hostLink!==null)?
-                                <a href={item.hostLink} target='_blank' className='text-secondary text-2xl bg-transparent'>
+                                <a href={item.hostLink} target='_blank' className='text-secondary text-xl lg:text-2xl bg-transparent'>
                                     <FaLink className='pointer'/>
                                 </a>
                             : null
                         }
                     </div>
-                    <p className='text-primary font-light text-center pt-3 px-6'>{item.desc}</p>
-                    <div className='flex flex-wrap gap-2 justify-center pt-2'>
+                    <p className='text-primary font-light text-center pt-3 lg:px-6'>{item.desc}</p>
+                    <div className='flex flex-wrap gap-2 justify-center pt-2 pb-10 lg:pb-0'>
                         {
                             item.techStack.map((techItem, idx) => (
-                                <div key={idx} className='flex w-fit bg-secondary rounded-xl justify-between px-3 items-center py-1'>
-                                    <i className={`${techItem.icon} pr-2 text-sm`}></i>
-                                    <p className='uppercase font-light text-sm'>{techItem.techName}</p>
+                                <div key={idx} className='flex w-fit bg-secondary rounded-xl justify-between px-2 lg:px-3 items-center py-1'>
+                                    <i className={`${techItem.icon} pr-2 text-xs lg:text-sm`}></i>
+                                    <p className='uppercase font-light text-xs lg:text-sm'>{techItem.techName}</p>
                                 </div>
                             ))
                         }
