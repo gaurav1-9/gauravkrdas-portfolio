4bd36d2159d8f324addf2c2ad68b765324953f0f
Gaurav <gauravkrdas19@gmail.com>
2025-06-10 01:31:06 +0530

    responsive for 2xl screens

--- src/PageComponents/About/RightInfo.jsx
+++ src/PageComponents/About/RightInfo.jsx
@@ -4,7 +4,7 @@ import TabbedInfo from './TabbedInfo'
 const RightInfo = () => {
     return (
         <div className='lg:h-full w-full lg:w-2/3 pt-6 md:pt-0 lg:pt-26 px-6 md:px-20 lg:pl-12 text-center lg:text-justify'>
-            <div className="lg:w-3xl">
+            <div className="lg:w-3xl 2xl:w-4xl 2xl:mt-5 min-[1536px]:mt-6">
                 <p className="font-light text-md lg:text-xl leading-4 lg:leading-6 text-primary">
                     Hey there! I'm <span className="text-secondary font-semibold">Gaurav Kumar Das</span>, a 23-year-old passionate coder from Assam, India. I specialize in <span className="font-medium italic">Python</span>, <span className="font-medium italic">full-stack web development</span>, and <span className="font-medium italic">Android development</span>.
                     I love building meaningful, interactive projects and have hands-on experience with <span className="italic font-medium">REST APIs, building Chatbots</span> and solving <span className="italic font-medium">challenging coding problems</span> that push my skills.
@@ -13,7 +13,7 @@ const RightInfo = () => {
                     I'm especially passionate about backend development, creative 3D modeling, and turning ideas into real-world solutions that make a difference.
                 </p>
             </div>
-            <TabbedInfo/>
+            <TabbedInfo />
         </div>
     )
 }
