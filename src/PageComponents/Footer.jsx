9fa140272783f55016e6bbc38186ca37577a0a73
Gaurav <gauravkrdas19@gmail.com>
2025-09-12 21:06:30 +0530

    footer data fixed

--- src/PageComponents/Footer.jsx
+++ src/PageComponents/Footer.jsx
@@ -8,8 +8,9 @@ const Footer = () => {
     return (
         <div className='bg-primary h-1/12 p-4 flex justify-center items-center'>
             <div className='flex items-center gap-1 text-primaryAccent'>
-                <LiaCopyright className='text-xs lg:text-base'/>
-                <p className='text-xs lg:text-base'>{year}, built with</p>
+                <LiaCopyright className='text-xs lg:text-base' />
+                <p className='text-xs lg:text-base'>
+                    {year === 2025 ? year : `2025 - ${year}`}, built with</p>
                 <FaHeart className='text-secondary text-xs lg:text-base' aria-label='love' />
                 <p className='text-xs lg:text-base'>by GAURAV KUMAR DAS</p>
             </div>
