9362749af7e6efb84fc7f3ee07c101815cbfbed5
Gaurav <gauravkrdas19@gmail.com>
2026-06-18 23:44:03 +0530

    dynamic project viewer

--- src/PageComponents/Projects/Projects.jsx
+++ src/PageComponents/Projects/Projects.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GithubChart from './GithubChart'
 import ProjectList from './ProjectList'
 import projectList from '../../data/projects.json'
 
 const Projects = () => {
+    const [visibleCount, setVisibleCount] = useState(5);
+
+    const visibleProjects = projectList.slice(0, visibleCount);
     return (
         <div className="w-full h-fit">
             <GithubChart />
-            <ProjectList projList={projectList} />
+            <ProjectList projList={visibleProjects} />
+
+            {
+                visibleCount < projectList.length &&
+                <div className='text-primaryAccent text-right px-15 pt-2 lg:pt-0 font-semibold lg:relative'>
+                    <button
+                        className='lg:absolute -top-6 right-45 cursor-pointer'
+                        onClick={() => setVisibleCount(prev => prev + 3)}
+                    >
+                        view more
+                    </button>
+                </div>
+            }
         </div>
     )
 }
