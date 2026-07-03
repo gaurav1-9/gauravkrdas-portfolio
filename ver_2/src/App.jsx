4b15c7713bd8208251f461088fe3cc60d14ba9df
Gaurav <gauravkrdas19@gmail.com>
2026-07-03 22:23:55 +0530

    colors and font setup

--- ver_2/src/App.jsx
+++ ver_2/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const App = () => {
   return (
-    <div className='bg-green-500 w-screen h-30'>Gaurav's portfolio</div>
+    <div className='bg-alabasterGrey h-300'>Gaurav's portfolio</div>
   )
 }
 
