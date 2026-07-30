75a56677416ab044ab7e5bce371cc3e24a1492db
Gaurav <gauravkrdas19@gmail.com>
2026-07-31 03:06:08 +0530

    v_p vite config updated

--- v_page_not_found/vite.config.js
+++ v_page_not_found/vite.config.js
@@ -5,10 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/(.*)",
-  server: {
-    fs: {
-      allow: [".."],
-    },
-  },
+  base: "/v_page_not_found/",
 })
