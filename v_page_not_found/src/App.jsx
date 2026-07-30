b24492dcf0e2fc54acd5bb7aceeaaad2f3f32504
Gaurav <gauravkrdas19@gmail.com>
2026-07-31 03:10:25 +0530

    responsiveness fixed

--- v_page_not_found/src/App.jsx
+++ v_page_not_found/src/App.jsx
@@ -7,12 +7,12 @@ const App = () => {
       <p className='text-6xl md:text-8xl font-semibold absolute top-25 md:top-55 lg:top-15 tracking-[20px]'>404</p>
       <img src={Gif404} className='mt-15 lg:mt-17 z-10' />
 
-      <p className='text-3xl md:text-5xl font-semibold pt-3'>Looks like you're lost</p>
-      <p className='text-base md:text-3xl lg:text-2xl font-light lg:py-2'>The page you are looking for is not available</p>
+      <p className='text-3xl md:text-5xl lg:text-4xl msi:text-5xl font-semibold pt-3'>Looks like you're lost</p>
+      <p className='text-base md:text-3xl lg:text-xl msi:text-2xl font-light msi:py-2'>The page you are looking for is not available</p>
 
       <a
         href="https://gauravkrdas-portfolio.vercel.app/"
-        className='text-xl md:text-3xl lg:text-2xl bg-onyx text-white py-2 md:py-5 lg:py-3 px-5 md:px-10 rounded-xl mt-10 hover:bg-onyx/95'
+        className='text-xl md:text-3xl lg:text-2xl bg-onyx text-white py-2 md:py-5 lg:py-3 px-5 md:px-10 rounded-xl mt-10 lg:mt-5 msi:mt-10 hover:bg-onyx/95'
       >
         <span>Let's go to a safe place</span>
       </a>
